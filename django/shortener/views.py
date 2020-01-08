479fedb2065ecc5e65c2ec63f96cc53bd61fea77
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2020-01-08 16:58:48 +0200

    Add get_client_ip function

--- django/shortener/views.py
+++ django/shortener/views.py
@@ -4,4 +4,4 @@ from .models import ShortURL
 
 class ShortURLRedirectView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
-        return ShortURL.objects.get(short_id=kwargs.get('short_id'))
+        return ShortURL.objects.get(short_id=kwargs.get('short_id')).redirect_url
