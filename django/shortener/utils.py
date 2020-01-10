61bd0f234d1672bb6b0a1ff1f9d7d7e62c290703
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2020-01-10 23:45:24 +0200

    Update client_ip function to use user's internal ip instead of proxy one

--- django/shortener/utils.py
+++ django/shortener/utils.py
@@ -1,7 +1,7 @@
 def client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[-1].strip()
+        ip = x_forwarded_for.split(',')[0].strip()
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
