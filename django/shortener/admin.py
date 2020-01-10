fc3c902cc27e6abe267369853a4381a701269ebf
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2020-01-10 16:06:47 +0200

    Update shortener admin

--- django/shortener/admin.py
+++ django/shortener/admin.py
@@ -6,6 +6,6 @@ from .models import ShortURL
 @admin.register(ShortURL)
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('short_id', 'redirect_url')
-    readonly_fields = ('short_id', 'creator_ip')
+    readonly_fields = ('short_id', 'creator_ip', 'transitions_number')
     list_filter = ('redirect_url',)
     search_fields = ('redirect_url',)
