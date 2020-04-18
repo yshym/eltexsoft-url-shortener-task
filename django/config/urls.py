36203c8dae855331d7d76d225e044da2e3a358be
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2020-04-18 03:22:02 +0300

    Use Swagger as documentation ui

--- django/config/urls.py
+++ django/config/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
-from rest_framework.documentation import include_docs_urls
 from rest_framework.authtoken import views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -19,6 +18,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('login/', views.obtain_auth_token, name='login'),
     path('admin/', admin.site.urls),
-    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='docs'),
     path('', include('shortener.urls')),
 ]
